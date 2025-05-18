063a0ee076fb61253bb09cceaba1b839fb3a4cf9
yashk230 <yashkaranjule230@gmail.com>
2025-05-18 12:14:46 +0530

    Update: bulk update across all repos

--- update.py
+++ update.py
@@ -3,7 +3,7 @@ import re
 
 TEMPLATES_DIR = "D:/work/templates"
 
-REPLACEMENT_HTML = '''<p class="mb-0 fs--1 text-700">&copy; This template is made with&nbsp;<svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path></svg>&nbsp;by&nbsp;<a class="text-700" href="https://dualsparkstudio.github.io/DualSparkStudio/" target="_blank">DualSpark Studio</a></p>'''
+REPLACEMENT_HTML = '''<p class="mb-0 fs--1 text-700">&copy; This template is made with&nbsp;<svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path></svg>&nbsp;by&nbsp;<a class="text-700" href="https://www.dualsparkstudio.com/" target="_blank">DualSpark Studio</a></p>'''
 
 def replace_in_html_files(root_dir):
     for subdir, _, files in os.walk(root_dir):
